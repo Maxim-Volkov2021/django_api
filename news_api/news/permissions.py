e38caf4b2dbd64aa99a3e157927b5e872545a484
max <k_r_o_n_o_s_@ukr.net>
2023-07-13 19:29:05 +0300

    little correction permission

--- news_api/news/permissions.py
+++ news_api/news/permissions.py
@@ -41,5 +41,7 @@ class IsOwnerOrReadOnlyForNotAuthor(permissions.BasePermission):
                 return True
         return False
     def has_object_permission(self, request, view, obj):
+        if request.user and request.user.is_staff:
+            return True
         if request.method in permissions.SAFE_METHODS and (request.user and request.user.is_staff):
             return True
